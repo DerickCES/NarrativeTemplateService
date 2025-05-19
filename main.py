963d45449e3d332e02989db23ede15237e2595c9
DerickCES <168730420+DerickCES@users.noreply.github.com>
2025-05-19 16:04:21 +0200

    port fix

--- main.py
+++ main.py
@@ -104,5 +104,5 @@ async def handle_get_templates():
         raise HTTPException(status_code=500, detail=f"Error fetching templates: {str(e)}")
     
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 3000))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
